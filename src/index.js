daf7a1be06301af2c2173d52ecf3ee314f89f3eb
Hugo Larcher <hugo.larcher@gmail.com>
2018-01-25 14:26:51 +0100

    Update Safari detection
    Update Safari detection for newer versions.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class BrowserDetection extends React.Component {
     const isOpera = (!!window.opr && !!opr.addons) || !!window.opera
                     || navigator.userAgent.indexOf(' OPR/') >= 0;
     const isFirefox = typeof InstallTrigger !== 'undefined';
-    const isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
     const isIE = /*@cc_on!@*/false || !!document.documentMode;
     const isEdge = !(isIE) && !!window.StyleMedia;
     const isChrome = !!window.chrome && !!window.chrome.webstore;
